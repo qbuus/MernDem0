26739335d7b3af20fb36952582e81261e8f39d1c
qbuus <jkocz867@gmail.com>
2023-03-07 12:11:40 +0100

    error handling and mongo schema model setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,38 @@
 import dotenv from "dotenv";
 dotenv.config();
-import express from "express";
+import express, {
+  NextFunction,
+  Request,
+  Response,
+} from "express";
+
+import NoteModel from "./models/note";
 
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("home");
+app.get("/", async function (req, res, next) {
+  try {
+    const notes = await NoteModel.find().exec();
+    res.status(200).json(notes);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use(function (req, res, next) {
+  next(Error("Endpoint not found"));
+});
+
+app.use(function (
+  error: unknown,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  console.log(error);
+  let errorMsg = "An error occured";
+  if (error instanceof Error) errorMsg = error.message;
+  res.status(500).json({ error: errorMsg });
 });
 
 export default app;
