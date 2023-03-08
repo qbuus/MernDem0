f6a4b9840020e95de258c535f3d72a1891b42f8a
qbuus <jkocz867@gmail.com>
2023-03-08 14:02:54 +0100

    proxy setup and fetch test

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,17 +1,25 @@
 import React from "react";
 import "./App.css";
-import { Button } from "react-bootstrap";
+import { Note } from "./models/note";
 
 function App() {
-  const [clickCount, setClickCount] = React.useState(0);
+  const [notes, setNotes] = React.useState<Note[]>([]);
 
-  return (
-    <Button
-      onClick={() => setClickCount((prev) => prev + 1)}
-    >
-      Click {clickCount} times
-    </Button>
-  );
+  React.useEffect(() => {
+    async function loadNotes() {
+      try {
+        const reponse = await fetch("/api/notes", { method: "GET" });
+        const notes = await reponse.json();
+        setNotes(notes);
+      } catch (error) {
+        console.error(error);
+        alert(error);
+      }
+    }
+    loadNotes();
+  }, []);
+
+  return <div className="App">{JSON.stringify(notes)};</div>;
 }
 
 export default App;
