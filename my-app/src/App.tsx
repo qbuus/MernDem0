97774c4a08e096ea9a4ffd49ad0cb219127b18ad
qbuus <jkocz867@gmail.com>
2023-03-08 14:24:47 +0100

    adding module styles and Note component

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import "./App.css";
-import { Note } from "./models/note";
+import { Note as NoteModel } from "./models/note";
+import Note from "./components/Note";
 
 function App() {
-  const [notes, setNotes] = React.useState<Note[]>([]);
+  const [notes, setNotes] = React.useState<NoteModel[]>([]);
 
   React.useEffect(() => {
     async function loadNotes() {
@@ -19,7 +19,13 @@ function App() {
     loadNotes();
   }, []);
 
-  return <div className="App">{JSON.stringify(notes)};</div>;
+  return (
+    <div>
+      {notes.map((note) => (
+        <Note note={note} key={note._id} />
+      ))}
+    </div>
+  );
 }
 
 export default App;
