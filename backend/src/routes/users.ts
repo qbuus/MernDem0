7a502ec0d5810f45aa967d63b7404ead5a5d13ab
qbuus <jkocz867@gmail.com>
2023-03-10 14:56:19 +0100

    bug fix where it was not able to logout

--- backend/src/routes/users.ts
+++ backend/src/routes/users.ts
@@ -6,6 +6,6 @@ const router = express.Router();
 router.post("/signup", userController.singUp);
 router.post("/login", userController.login);
 router.get("/", userController.getAuth);
-router.post("logout", userController.logout);
+router.post("/logout", userController.logout);
 
 export default router;
